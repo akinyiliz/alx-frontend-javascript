5765bbb79ca0dcaea484492ee9d8a4ba244bbb44
akinyiliz <lizzieke84@gmail.com>
2023-01-25 22:49:51 +0300

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
+    if(success) {
       resolve({ status: 200, body: 'Success' });
-    }
-    else {
+    } else {
       reject(Error('The fake API is not working currently'));
     }
   });
