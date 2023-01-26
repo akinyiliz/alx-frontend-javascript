05f6afb38b025db4e7c3ae4def16e72bb7407ed0
akinyiliz <lizzieke84@gmail.com>
2023-01-27 00:32:23 +0300

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,8 +3,8 @@ import Currency from "./3-currency";
 
 export default class Pricing {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   // amount getter
